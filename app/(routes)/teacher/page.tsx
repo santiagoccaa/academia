a40fa2941fa1b5a2f06e7cb4865a4d6669e7666b
santiagoccaa <santiagocontrerasarrieta@gmail.com>
2026-03-12 08:24:48 -0500

    prisma instalado correctamente

--- app/(routes)/teacher/page.tsx
+++ app/(routes)/teacher/page.tsx
@@ -1,9 +1,18 @@
 import { currentUser } from "@clerk/nextjs/server"
 import { Header } from "./components"
+import prisma from "@/lib/prisma"
 
 export default async function TeacherPage() {
     const user = await currentUser()
     if (!user) return
+
+    const courses = await prisma.course.findMany({
+        where: {
+            userID: user.id
+        }
+    })
+
+    console.log("cursor",courses);
 
     return (
         <div>
