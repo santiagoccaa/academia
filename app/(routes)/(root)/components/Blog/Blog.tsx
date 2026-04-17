179161aef3dffb8152777ea207b7890383737444
santiagoccaa <santiagocontrerasarrieta@gmail.com>
2026-04-17 14:40:40 -0500

    styles: blog direction styles

--- app/(routes)/(root)/components/Blog/Blog.tsx
+++ app/(routes)/(root)/components/Blog/Blog.tsx
@@ -5,13 +5,13 @@ import { blogs } from './blog.data'
 export const Blog = () => {
     return (
         <PageContainer>
-            <div className='flex flex-col my-8'>
+            <div className='flex flex-col my-8 mb-16'>
                 <h2 className='text-sm font-medium text-violet-500'>Blog</h2>
                 <p className='text-3xl font-medium text-gray-800 max-w-xl'>
                     Our recent blogs
                 </p>
                 <div className='flex flex-col lg:flex-row gap-8 lg:gap-4 mt-8'>
-                    <div className='w-full lg:w-1/2 flex flex-col lg:flex-row space-y-8 lg:space-x-4'>
+                    <div className='w-full lg:w-1/2 flex flex-col space-y-8 lg:space-x-4'>
                         <CardBlog {...blogs[0]} />
                         <CardBlog {...blogs[1]} />
                     </div>
