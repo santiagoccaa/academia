6d783ecad381d074962fed73b0f55217fcbb6fef
santiagoccaa <santiagocontrerasarrieta@gmail.com>
2026-04-28 11:56:38 -0500

    filter done

---
+++ components/Shared/FilterCourse/FilterCourse.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import {
+    Combobox,
+    ComboboxContent,
+    ComboboxEmpty,
+    ComboboxInput,
+    ComboboxItem,
+    ComboboxList,
+} from "@/components/ui/combobox"
+import { FilterCourseProps } from "./FilterCourse.types"
+import { useRouter } from "next/navigation"
+
+
+export function FilterCourse({ filter }: FilterCourseProps) {
+
+    const router = useRouter()
+    return (
+        <Combobox items={filter}>
+            <ComboboxInput placeholder="Buscar curso" />
+            <ComboboxContent>
+                <ComboboxEmpty>Curso no encontrado</ComboboxEmpty>
+                <ComboboxList>
+                    {(item) => (
+                        <ComboboxItem
+                            key={item.slug}
+                            value={item.names}
+                            onClick={() => router.push(`/academy/courses/${item.slug}`)}
+                        >
+                            {item.names}
+                        </ComboboxItem>
+                    )}
+                </ComboboxList>
+            </ComboboxContent>
+        </Combobox >
+    )
+}
--- components/Shared/ListCourses/ListCourses.tsx
+++ components/Shared/ListCourses/ListCourses.tsx
@@ -1,12 +1,26 @@
 import { CardCourse } from '../CardCourse'
+import { FilterCourse } from '../FilterCourse'
 import { ListCoursesProps } from './ListCourses.types'
 
 export const ListCourses = ({ courses, title }: ListCoursesProps) => {
 
+    const nameCourses = courses?.map(course => {
+        const names = course.title
+        const slug = course.slug
+
+        return {
+            names,
+            slug
+        }
+    })
+
     return (
         <div className=''>
             <div className='my-4 mx-6 border rounded-lg bg-white p-6'>
-                <h2 className='text-2xl font-normal'>{title}</h2>
+                <div className='w-full flex items-center justify-between pb-2'>
+                    <h2 className='text-2xl font-normal'>{title}</h2>
+                    {nameCourses && <FilterCourse filter={nameCourses} />} 
+                </div>
                 <div className='border-t py-2'>
                     {courses && courses.length > 0 ? (
                         <div className='grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'>
