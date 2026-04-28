61d8c76bd49eed53b98568052c92d4a177e653d2
santiagoccaa <santiagocontrerasarrieta@gmail.com>
2026-04-28 17:27:17 -0500

    home: hero translations

--- components/Shared/LenguajeSelector/LenguajeSelector.tsx
+++ components/Shared/LenguajeSelector/LenguajeSelector.tsx
@@ -12,31 +12,24 @@ import {
 } from "@/components/ui/dropdown-menu"
 import setLangCookie from "@/i18n/setLangCookie";
 import { Languages } from "lucide-react";
-import { useLocale } from "next-intl";
-import { useState } from "react";
-
-const languajes = ["es", "en"]
 
 export const LanguageSelector = () => {
-    const lang = useLocale();
 
     const handleSelectLang = (lang: string) => {
         setLangCookie(lang);
     };
     return (
-        <>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="outline" className="rounded-full shadow-none" ><Languages size={25} /></Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent >
-                    <DropdownMenuGroup>
-                        <DropdownMenuItem onClick={() => handleSelectLang("es")}>Es</DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => handleSelectLang("en")}>En</DropdownMenuItem>
-                    </DropdownMenuGroup>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="rounded-full shadow-none" ><Languages size={25} /></Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent >
+                <DropdownMenuGroup>
+                    <DropdownMenuItem onClick={() => handleSelectLang("es")}>Es</DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => handleSelectLang("en")}>En</DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 };
 
